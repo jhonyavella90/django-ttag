82f86d71afd5d8010a205d46db49b9b202450f8c
Chris Beaven <smileychris@gmail.com>
2011-05-27 16:08:16 +1200

    1.0 release

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'ttag.helpers',
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
